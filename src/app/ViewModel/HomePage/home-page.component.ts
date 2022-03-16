fed4a1dfc6964ad994562a39d2eb14b32acacdfa
BacemBenDaly <bendalybacem@gmail.com>
2022-03-16 10:34:46 +0100

    Home Page + Structure ✅

---
+++ src/app/ViewModel/HomePage/home-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-page',
+  templateUrl: '../../View/HomePage/home-page.component.html',
+  styleUrls: ['../../View/HomePage/home-page.component.css']
+})
+export class HomePageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
